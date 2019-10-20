f5a67d5c4ade74230757bfc53347d2782ade6a03
leventelibal <leventelibal@gmail.com>
2019-10-19 21:29:36 -0400

    updated variable.tf chart, templaadd wordpress.tf

---
+++ deployment/terraform/fuchicorp-wordpress/variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  default = "fuchicorp-wordpress"
+}
+variable "fuchicorp" {
+    default = "./fuchicorp-wordpress"
+  
+}
+variable "version" {
+    default = "6.0.1"
+  
+}
+variable "deployment_image" {
+  default = "wordpress"
+  
+}
+  
+
+variable "deployment_environment" {
+  default = "dev"
+}
+
+
+variable "deployment_endpoint" {
+  type = "map"
+
+  default = {
+    dev  = "dev.wordpress.fuchicorp.com"
+    qa   = "qa.wordpress.fuchicorp.com"
+    prod = "wordpress.fuchicorp.com"
+  }
+}
