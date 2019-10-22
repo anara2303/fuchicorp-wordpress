4b10b98476528707f53200f683703760f570ebfd
Farkhod <sadykovfarkhod@gmail.com>
2019-10-22 18:34:17 -0500

    added deployment name to the helm chart

--- deployment/terraform/wordpress.tf
+++ deployment/terraform/wordpress.tf
@@ -1,9 +1,9 @@
 data "template_file" "fuchicorp_wordpress_values" {
   template = "${file("./fuchicorp-wordpress/template_values.yaml")}"
-  vars = {
-    docker_image = "${var.deployment_image}"
-    deployment_endpoint = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
 
+  vars = {
+    docker_image        = "${var.deployment_image}"
+    deployment_endpoint = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
   }
 }
 
@@ -11,13 +11,14 @@ resource "local_file" "fuchicorp_wordpress_values_local_file" {
   content  = "${trimspace(data.template_file.fuchicorp_wordpress_values.rendered)}"
   filename = "./fuchicorp-wordpress/.cache/values.yaml"
 }
+
 resource "helm_release" "fuchicorp_wordpress" {
-  name       = "${var.name}"
-  chart      = "${var.chart}"
-  version    = "${var.version}"
+  name      = "${var.name}-${var.deployment_environment}"
+  chart     = "${var.chart}"
+  version   = "${var.version}"
   namespace = "${var.deployment_environment}"
 
- values = [
-    "${data.template_file.fuchicorp_wordpress_values.rendered}"
+  values = [
+    "${data.template_file.fuchicorp_wordpress_values.rendered}",
   ]
 }
